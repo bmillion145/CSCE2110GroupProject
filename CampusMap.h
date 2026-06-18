f8299628ee0157964be29c3d3937f463d495b714
andrewsaul772-commits <andrewsaul772@gmail.com>
2026-06-18 10:31:10 -0500

    Update CampusMap.h

---
+++ CampusMap.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "FileManager.h"
+class CampMap {
+public:
+	//reads the map file and adds it to a dynamic 2d array
+	void readMapFile();
+	//takes user chordinates and tells them the ID of the current and adjacent locations 
+	void findMe();
+	//decodes the map characters into their repective tile types
+	string deCode(int x, int y);
+	//deleted the dynamicly allocated array
+	~CampMap();
+	//prints out the map
+	void printMap();
+
+
+private:
+	int numOfRow = 0, numOfCol = 0;
+	//2d array 
+	char** gridMap;
+	//in file and file system 
+	ifstream rFile;
+	FileManager readFile;
+};
