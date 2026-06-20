337845264d40df8badfd2a603be7f10569fea418
andrewsaul772-commits <andrewsaul772@gmail.com>
2026-06-20 03:47:36 -0500

    Update CampusMap.cpp

--- CampusMap.cpp
+++ CampusMap.cpp
@@ -6,9 +6,9 @@ void CampMap::readMapFile() {
 	readFile.setFile();
 	//sets in file stream
 	rFile.open(readFile.getName());
-	//takes in the number of rows and collems from the in file
+	//takes in the number of rows and columns from the in file
 	rFile >> numOfRow >> numOfCol;
-	//sets the array to a size based on the specified rows and collems
+	//sets the array to a size based on the specified rows and columns
 	gridMap = new char* [numOfRow];
 	for (int i = 0; i < numOfRow; ++i) {
 		gridMap[i] = new char[numOfCol];
@@ -21,20 +21,20 @@ void CampMap::readMapFile() {
 	}
 }
 
-	//takes user chordinates and tells them the ID of the current and adjacent locations 
+	//takes user coordinates and tells them the ID of the current and adjacent locations 
 	void CampMap::findMe() {
 		int x = 0, y = 0;
 		bool blocked = true;
-		//taking input for user chordinates
+		//taking input for user coordinates
 		while (blocked) {
-			cout << "Ender X and Y chordinate respectivly: ";
+			cout << "Ender X and Y coordinate respectively: ";
 			cin >> x >> y;
 			// checking to make sure a valid set was entered
 			if (deCode(x, y) == "Blocked") {
-				cout << "Area entered is marked as blocked please enter a valid chordiante set\n";
+				cout << "Area entered is marked as blocked, please enter a valid chordiante set\n";
 			}
-			else if (deCode(x, y) == "Unkown" || deCode(x, y) == "Off Map") {
-				cout << "Area is invalid or off the designated map grid please enter a valid chordinate set";
+			else if (deCode(x, y) == "Unknown" || deCode(x, y) == "Off Map") {
+				cout << "Area is invalid or off the designated map grid, please enter a valid coordinate set\n";
 			}
 			else {
 				blocked = false;
@@ -50,11 +50,11 @@ void CampMap::readMapFile() {
 		cout << "Left: " << deCode(x -1 , y ) << endl;
 		cout << "Right: " << deCode(x +1, y ) << endl;
 	}
-	//decodes the map characters into their repective tile types
+	//decodes the map characters into their respective tile types
 	string CampMap::deCode(int x, int y) {
 		char toDeCode;
 		//checks to makes sure value is inside of the map stops out of range errors
-		if (x < 0 || x > numOfCol || y < 0 || y > numOfRow) {
+		if (x < 0 || x >= numOfCol || y < 0 || y >= numOfRow) {
 			return "Off Map";
 		}
 		//takes the x y and sorts uses a switch case to return the proper text relating the the character
@@ -79,13 +79,13 @@ void CampMap::readMapFile() {
 			return "Blocked Area";
 			break;
 		default:
-			return "unkown";
+			return "unknown";
 		}
-		//here for extra error hanling just in case
-		return "Unkown";
+		//here for extra error handling just in case
+		return "Unknown";
 	}
 
-	//deleted the dynamicly allocated array
+	//deleted the dynamically allocated array
 	CampMap::~CampMap() {
 		for (int i = 0; i < numOfRow; ++i) {
 			delete[] gridMap[i];
@@ -96,7 +96,7 @@ void CampMap::readMapFile() {
 
 	//prints out the map
 	void CampMap::printMap() {
-		//sets the top collem number indicators
+		//sets the top column number indicators
 		cout << "  ";
 		for (int i = 0; i < numOfCol; ++i) {
 			cout << i;
