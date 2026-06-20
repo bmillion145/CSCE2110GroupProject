424b619383d589c40d0707dc64b9539c5efb0b3e
andrewsaul772-commits <andrewsaul772@gmail.com>
2026-06-20 03:36:16 -0500

    Update StudentNode.h

--- StudentNode.h
+++ StudentNode.h
@@ -82,17 +82,5 @@ public:
 
 
 };
-    //handles the retrival and validation of in files
-class FileManager {
-public:
-	//sets the file instream through taking a file name from user input
-	void setFile();
-	//returns the file name of a validated file
-	string getName();
-
-private:
-	ifstream inFile;
-	string fileName;
-};
 #endif
 
