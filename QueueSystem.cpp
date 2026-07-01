e579e5e819d189893c625e5283deccb67007ff39
andrewsaul772-commits <andrewsaul772@gmail.com>
2026-07-01 15:37:53 -0500

    added comments to code

--- QueueSystem.cpp
+++ QueueSystem.cpp
@@ -1,22 +1,27 @@
 #include "QueueSystem.h"
 
-
+//gets the information to create and add a new student struct to the advising queue
 void AdvisingQueue::addToQueue() {
 	student tempS;
 	string inIssue;
+	//geting sutdent info
 	cout << "Please enter student ID: ";
 	cin >> tempS.id;
 	cin.ignore();
-	cout << "Please enter advissing issue: ";
+	cout << "Please enter advising issue: ";
 	getline(cin, inIssue);
 	tempS.issue = inIssue;
+	//pusing the student into the queue
 	advQueue.push(tempS);
 	cout << "Student was added " << advQueue.size() << ": place in queue\n";
 }
 
+//processes the student in the queue by displaying info and removing them from queue
 void AdvisingQueue::processNext() {
+	//printing student information
 	cout << "Student ID: " << advQueue.front().id << endl;
 	cout << "Advising issue: " << advQueue.front().issue << endl;
+	//asking the user if the issue was resolved if yes, the student is removed from the queue
 	cout << "was the issue resolved |y| |n| : ";
 	char choice;
 	cin >> choice;
@@ -25,14 +30,14 @@ void AdvisingQueue::processNext() {
 		cout << "Student was removed form queue\n";
 	}
 	else if (choice == 'n' || choice == 'N') {
-		cout << "keeping student at front of queue\n";
+		cout << "Keeping student at front of queue\n";
 	}
 }
-
+//returns the size of the queue
 int AdvisingQueue::getQueueSize() {
 	return advQueue.size();
 }
-
+//prints all the information of each student in the queue
 void AdvisingQueue::printAll() {
 	queue<student> tempQueue;
 	tempQueue = advQueue;
