4bde70b3b95aa6cbb22efa62b1c48813d92f2382
andrewsaul772-commits <andrewsaul772@gmail.com>
2026-07-01 15:39:52 -0500

    added comments to code

--- QueueSystem.h
+++ QueueSystem.h
@@ -5,11 +5,17 @@ using namespace std;
 
 class AdvisingQueue {
 public:
+	//adds student to queue
 	void addToQueue();
+	//prints next in queue data and removes the student from queue
 	void processNext();
+	//returns the queue size
 	int getQueueSize();
+	//prints all information of each student in queue
 	void printAll();
 private:
+	//defines what a student is
 	struct student { int id = 0; string issue = ""; };
+	//declaring queue stl
 	queue<student> advQueue;
 };
